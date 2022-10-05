fd971f887b318cffa180d6c162a9d4b85d1b255d
Aayushi Bhargava <aayushi.bhargava1910@gmail.com>
2022-10-05 00:43:57 -0700

    first commit

--- js/activity.js
+++ js/activity.js
@@ -1,14 +1,14 @@
-$(function () {
-  $(".highlight td").click(function () {
-    var cell = $(".highlight td").index(this);
-    if (cell != 0) {
-      var str = $(this).text();
-      if (str != "Not Available") {
-        $(this).toggleClass("greenBackground");
-      }
-    }
-  });
-});
+// $(function () {
+//   $(".highlight td").click(function () {
+//     var cell = $(".highlight td").index(this);
+//     if (cell != 0) {
+//       var str = $(this).text();
+//       if (str != "Not Available") {
+//         $(this).toggleClass("greenBackground");
+//       }
+//     }
+//   });
+// });
 
 $(function () {
   $(".highlight td").hover(function () {
@@ -21,3 +21,28 @@ $(function () {
     }
   });
 });
+
+//User interaction with table cells//
+
+
+$("td").click(function () {
+  var content = $(this).text();
+
+  if (content != "Not Available") {
+    $(this).toggleClass("greenBackground");
+
+    if ($(this).hasClass("greenBackground")) {
+      $("#displaySelected").css("visibility", "visible");
+      $("#displaySelected").css("margin-top", "2em");
+      $("#result").append("<p>" + content + "</p>");
+    } else {
+      $("#result p:contains(" + content + ")").remove();
+
+      if ($("#result").has("p").length == false) {
+        $("#displaySelected").css("visibility", "hidden");
+        $("#displaySelected").css("margin-top", "0");
+      }
+    }
+  }
+});
+
